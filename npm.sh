b555b5d7fa5932d89786478b4c02eaddc8955add
永雏塔菲 <108621198+taffychan@users.noreply.github.com>
2022-07-07 13:14:41 +0800

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -50,9 +50,11 @@ fi
 if [[ -z (type -P docker) ]]; then
     curl -fsSL https://get.docker.com | bash -s docker
 fi
-curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+if [[ -z (type -P docker-compose) ]]; then
+    curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+fi
 
 cd /home
 if [[ ! -d mydocker ]]; then
